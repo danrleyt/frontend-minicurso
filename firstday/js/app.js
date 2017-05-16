ae753e996d2fa70bb01c61b98fc0265e417abeed
Dan <danrleyt@gmail.com>
2017-05-16 16:28:12 -0300

    novo commit

--- firstday/js/app.js
+++ firstday/js/app.js
@@ -4,7 +4,8 @@ angular.module('filmeApp',[])
       var url = 'https://api.themoviedb.org/3/movie/550?api_key=e5db9bda9b7cd341ff6374e57b2608a0';
       $http.get(url).then(
         function(dados){
-          console.log(dados);
+          //console.log(dados.data);
+          $scope.filme = dados.data;
         },
         function(erro){
           console.log(erro);
