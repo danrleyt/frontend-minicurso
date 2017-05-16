3ca08687a729406b9a27934624403c99ff26233c
Dan <danrleyt@gmail.com>
2017-05-16 16:13:36 -0300

    novo commit

--- firstday/js/app.js
+++ firstday/js/app.js
@@ -1,6 +1,9 @@
 angular.module('filmeApp',[])
-  .controller('FilmeCtrl', ['$scope', function($scope){
+  .controller('FilmeCtrl', ['$scope','$http', function($scope, $http){
     $scope.teste = function(){
         alert("OLÁ PESSOAL!");
     };
+
+    var url = 'https://api.themoviedb.org/3/movie/550?api_key=e5db9bda9b7cd341ff6374e57b2608a0';
+    $http.get();
   }])
